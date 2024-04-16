65347b2bb0e681b02315f1d216b0cf387e1e8848
Gabriel Cavalheiro <gabriel.cavalheiro@nexxera.com>
2024-04-16 16:19:02 -0300

    [main] feat: declare compatibility with HPOS

--- woocommerce-nixpay.php
+++ woocommerce-nixpay.php
@@ -13,6 +13,10 @@
  * Requires at least: 5.4
  * Tested up to: 6.4.3
  *
+ * Requires Plugins: woocommerce
+ * WC requires at least: 8.6.0
+ * WC tested up to: 8.6
+ *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -29,12 +33,13 @@ use Nix\WoocommerceNixpay\StartWooCommerceNixPay;
 use Nix\WoocommerceNixpay\Utils\PathUtils;
 
 add_action( 'before_woocommerce_init', function () {
+	FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__ );
 	FeaturesUtil::declare_compatibility( 'cart_checkout_blocks', __FILE__ );
 } );
 
 StartWooCommerceNixPay::init();
-
 if ( ! class_exists( 'PathUtils' ) ) {
 	$GLOBALS['path_utils'] = new PathUtils();
 }
+
 
